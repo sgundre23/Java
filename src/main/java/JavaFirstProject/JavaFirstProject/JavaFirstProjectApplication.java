af6814458faa781ae922658f253a7da6f112c02d
SandhyaGundre <sandhyagundre@gmail.com>
2023-04-04 22:42:48 -0400

    first commit

---
+++ src/main/java/JavaFirstProject/JavaFirstProject/JavaFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package JavaFirstProject.JavaFirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFirstProjectApplication.class, args);
+	}
+
+}
